093760e0cb5ca5d020df37ae26e3f7dcd0e26434
Stefanija96 <stefanija96@yahoo.com>
2022-04-12 21:31:34 +0200

    models

---
+++ CRM.App/CRM.Domain/Enums/ProductionEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Domain.Enums
+{
+    public enum ProductionEnum
+    {
+        ProductionDirector=1,
+        SeniorProductionManager,
+        ProductionManager,
+        SeniorProducer,
+        Producer
+
+
+
+
+    }
+}
